7074d376c269e949c22a46ba4e0f853212971311
blancior <pkedziora347@gmail.com>
2024-05-02 14:05:48 +0200

    Feat: Created user validation middleware

---
+++ backend/middlewares/validateUser.js
@@ -0,0 +1,14 @@
+import {UserModel} from "../models/userModel.js";
+
+export async function validateRequest(userId) {
+    try {
+        const user = await UserModel.findById(userId);
+        if (!user) {
+            return [false, 'This user does not exist!'];
+        }
+        return [true, 'This user exists.'];
+    } catch (error) {
+        console.error(error);
+        return [false, 'An error occurred while validating user.'];
+    }
+}
