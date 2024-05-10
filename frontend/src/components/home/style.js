1d99e9244f6de385063a5a7687247425874580ea
blancior <pkedziora347@gmail.com>
2024-05-10 12:48:05 +0200

    Style: home component styling

---
+++ frontend/src/components/home/style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    font-size: 3rem;
+    //text-shadow: -5px 5px black;
+    flex-direction: column;
+    align-items: center;
+    height: 100vh;
+    color: antiquewhite;
+`
+export const InfoBar = styled.div`
+    margin-bottom: 4vh;
+`
