1ff0ae7d5ea09288961f8ec42a574ff3375e8850
blancior <pkedziora347@gmail.com>
2024-05-12 21:13:19 +0200

    Feat: set CORS headers & installed axios

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,12 @@ const PORT = 5000;
 const MONGODB_URI = process.env.DB_ADDRESS;
 
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // Ustawia dozwolone źródło na adres Twojej aplikacji React
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 app.use(cors());
 app.use("/", routes);
 mongoose.connect(MONGODB_URI).then(()=>{
