371f83e027ffc746e1923eb90f45edc1ce3a1ebb
blancior <pkedziora347@gmail.com>
2024-05-18 19:52:28 +0200

    Style:user registration style

---
+++ frontend/src/components/register/style.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const InputLabel = styled.div`
+    min-height: 8vh;
+`
+export const FormContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+`
+export const Form = styled.form`
+    background: aliceblue;
+    opacity: .8;
+    width: 25vw;
+    padding: 1vh 0;
+    border-radius: 10px;
+    p{
+        text-shadow:none;
+    }
+`
